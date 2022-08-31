644b88974047a2049403c7290ed67a768153e732
voltux <ioannis.arapis@ovhcloud.com>
2022-08-31 14:04:37 +0200

    [nvim] move treesitter config to its own file

---
+++ nvim/.config/nvim/lua/treesitter_conf.lua
@@ -0,0 +1,11 @@
+require('orgmode').setup_ts_grammar()
+
+-- Tree-sitter configuration
+require'nvim-treesitter.configs'.setup {
+  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
