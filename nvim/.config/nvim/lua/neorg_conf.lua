278d165edfa1496e27ff70c43d88d9c558cbaeaa
voltux <voltux@falcon.com>
2022-10-09 16:24:57 +0200

    [nvim] [neorg] check if todo dir exists in order to load gtd associated stuff

--- nvim/.config/nvim/lua/neorg_conf.lua
+++ nvim/.config/nvim/lua/neorg_conf.lua
@@ -1,41 +1,71 @@
-require('neorg').setup {
-    load = {
-        ["core.defaults"]               = {},
-        ["core.integrations.telescope"] = {},
-        ["core.integrations.nvim-cmp"]  = {},
-        ["core.norg.dirman"]            = {
-            config = {
-                workspaces = {
-                    todo = os.getenv("HOME") .. "/Documents/todo"
-                },
-                index = "index.norg"
-            }
-        },
-        ["core.gtd.base"]               = {
-            config = {
-                workspace = "todo"
-            },
-        },
-        ["core.gtd.ui"]                 = {},
-        ["core.gtd.helpers"]            = {},
-        ["core.gtd.queries"]            = {},
-        ["core.norg.completion"]        = {
-            config = {
-                engine = "nvim-cmp"
-            }
-        },
-        ["core.norg.concealer"]         = {},
-        ["core.norg.journal"]           = {},
-        ["core.presenter"]              = {
-            config = {
-                zen_mode = "truezen"
-            }
-        },
-        ["core.export.markdown"]        = {},
-        ["core.norg.manoeuvre"]         = {},
-        ["core.export"]                 = {},
+-- helper functions
+--- Check if a file or directory exists in this path
+function exists(file)
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            -- Permission denied, but it exists
+            return true
+        end
+    end
+    return ok, err
+end
 
-    }
+--- Check if a directory exists in this path
+function isdir(path)
+    -- "/" works on both Unix and Windows
+    return exists(path .. "/")
+end
+
+local basic_load = {
+    ["core.defaults"]               = {},
+    ["core.integrations.telescope"] = {},
+    ["core.integrations.nvim-cmp"]  = {},
+    ["core.norg.dirman"]            = {
+        config = {
+            workspaces = {
+                todo = os.getenv("HOME") .. "/Documents/todo"
+            },
+            index = "index.norg"
+        }
+    },
+    ["core.norg.completion"]        = {
+        config = {
+            engine = "nvim-cmp"
+        }
+    },
+    ["core.norg.concealer"]         = {},
+    ["core.norg.journal"]           = {},
+    ["core.presenter"]              = {
+        config = {
+            zen_mode = "truezen"
+        }
+    },
+    ["core.export.markdown"]        = {},
+    ["core.norg.manoeuvre"]         = {},
+    ["core.export"]                 = {},
+}
+
+local gtd_load = {
+    ["core.gtd.base"]    = {
+        config = {
+            workspace = "todo"
+        },
+    },
+    ["core.gtd.ui"]      = {},
+    ["core.gtd.helpers"] = {},
+    ["core.gtd.queries"] = {},
+}
+
+local load = basic_load
+if isdir(os.getenv("HOME") .. "/Documents/todo") then
+    for k, v in pairs(gtd_load) do
+        load[k] = v
+    end
+end
+
+require('neorg').setup {
+    load = load
 }
 local neorg_callbacks = require("neorg.callbacks")
 
