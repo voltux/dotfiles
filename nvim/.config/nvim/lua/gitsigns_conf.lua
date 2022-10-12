6c0464c9af338cc803139d74e7597cb87d7e3342
voltux <ioannis.arapis@ovhcloud.com>
2022-10-12 15:16:14 +0200

    [nvim] gitsigns configuration

--- nvim/.config/nvim/lua/gitsigns_conf.lua
+++ nvim/.config/nvim/lua/gitsigns_conf.lua
@@ -1,3 +1,40 @@
-require('gitsigns').setup()
-vim.api.nvim_set_keymap('n', ']g', '<cmd>Gitsigns next_hunk<cr>', { noremap = true, desc = 'Gitsigns go to next hunk' })
-vim.api.nvim_set_keymap('n', '[g', '<cmd>Gitsigns prev_hunk<cr>', { noremap = true, desc = 'Gitsigns go to previous hunk' })
+require('gitsigns').setup {
+    on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+        end, { expr = true, desc = 'Gitsigns go to next hunk' })
+
+        map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+        end, { expr = true, desc = 'Gitsigns go to previous hunk' })
+
+        -- Actions
+        map({ 'n', 'v' }, '<leader>gs', ':Gitsigns stage_hunk<CR>', {desc = 'Gitsigns stage hunk'})
+        map({ 'n', 'v' }, '<leader>gr', ':Gitsigns reset_hunk<CR>', {desc = 'Gitsigns reset hunk'})
+        map('n', '<leader>gS', gs.stage_buffer, {desc = 'Gitsigns stage buffer'})
+        map('n', '<leader>gu', gs.undo_stage_hunk, {desc = 'Gitsigns undo stage hunk'})
+        map('n', '<leader>gR', gs.reset_buffer, {desc = 'Gitsigns reset buffer'})
+        map('n', '<leader>gv', gs.preview_hunk, {desc = 'Gitsigns preview hunk'})
+        map('n', '<leader>gB', function() gs.blame_line { full = true } end, {desc = 'Gitsigns blame line'})
+        map('n', '<leader>gT', gs.toggle_current_line_blame, {desc = 'Gitsigns toggle current line blame'})
+        map('n', '<leader>gd', gs.diffthis, {desc = 'Gitsigns diff this'})
+        map('n', '<leader>gD', function() gs.diffthis('~') end, {desc = 'Gitsigns diff this $HOME'})
+        map('n', '<leader>gt', gs.toggle_deleted, {desc = 'Gitsigns toggle deleted'})
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    end
+}
