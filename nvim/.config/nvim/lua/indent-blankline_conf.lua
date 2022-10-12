4c720edf7c9168d90fc4ca0986d3835d32bebb77
voltux <ioannis.arapis@ovhcloud.com>
2022-10-12 14:16:08 +0200

    [nvim] modify keymaps to toggle plugins

--- nvim/.config/nvim/lua/indent-blankline_conf.lua
+++ nvim/.config/nvim/lua/indent-blankline_conf.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap('n', '<leader>i', ':IndentBlanklineToggle<CR>',
+vim.api.nvim_set_keymap('n', '<leader>pi', ':IndentBlanklineToggle<CR>',
     { noremap = true, desc = 'IndentBlankLine Toggle' })
 require('indent_blankline').setup {
     filetype_exclude = { 'dashboard' }
