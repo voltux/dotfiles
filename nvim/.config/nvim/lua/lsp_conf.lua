9f4689e2eb9118339c51a4e61da21452b08f25cb
voltux <voltux@falcon.com>
2022-10-15 22:46:58 +0200

    [nvim] cmp-lsp update deprecated function call

--- nvim/.config/nvim/lua/lsp_conf.lua
+++ nvim/.config/nvim/lua/lsp_conf.lua
@@ -59,7 +59,7 @@ local custom_attach = function(client, bufnr)
 end
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- setup servers for each programming language
 lsp.pylsp.setup {
