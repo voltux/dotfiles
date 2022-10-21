f9283d53d0a304eeddce13eee8a4b487d287d66d
voltux <ioannis.arapis@ovhcloud.com>
2022-10-21 12:34:01 +0200

    [nvim] (feat)slime recognise last entered terminal: create autocommand to update g:slime_last_channel on TermEnter and b:slime_config on BufEnter

--- nvim/.config/nvim/lua/vim-slime_conf.lua
+++ nvim/.config/nvim/lua/vim-slime_conf.lua
@@ -1,1 +1,14 @@
 vim.g.slime_target = "neovim"
+
+-- autocmd to assign last entered terminal as slime target
+vim.api.nvim_create_autocmd('TermEnter', {
+    pattern = '*',
+    command = 'let g:slime_last_channel=b:terminal_job_id',
+})
+vim.api.nvim_create_autocmd('BufEnter', {
+    pattern = '*',
+    command = [[if exists("b:slime_config")
+                    let b:slime_config = {"jobid": get(g:, "slime_last_channel", b:slime_config["jobid"])}
+                endif
+                ]],
+})
