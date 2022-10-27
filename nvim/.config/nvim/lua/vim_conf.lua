ede7cd9b99a1cedae9e3be1efa369ccdbf06aa60
voltux <ioannis.arapis@ovhcloud.com>
2022-10-27 14:19:09 +0200

    [nvim] quickfix lua shell check

--- nvim/.config/nvim/lua/vim_conf.lua
+++ nvim/.config/nvim/lua/vim_conf.lua
@@ -87,7 +87,7 @@ vim.api.nvim_set_keymap('t', '<C-Space>', '<C-\\><C-n>',
 vim.cmd [[tnoremap <expr> <C-W>" '<C-\><C-N>"'.nr2char(getchar()).'pi']]
 vim.cmd [[ autocmd TermOpen * setlocal nonumber norelativenumber signcolumn=no ]]
 vim.cmd [[ autocmd TermOpen * startinsert ]]
-if vim.fn.executable("zsh") then
+if vim.fn.executable("zsh")>0 then
     vim.o.shell = "zsh"
 end
 
