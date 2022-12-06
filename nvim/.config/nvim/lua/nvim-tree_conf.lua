81306b5d5fd37bf8fc918ce7be892ec71f3612d9
voltux <ioannis.arapis@ovhcloud.com>
2022-12-06 12:53:51 +0100

    [nvim] remove signcolumn from nvim-tree
    removing signcolumn from nvim-tree as it takes unnecessary space

--- nvim/.config/nvim/lua/nvim-tree_conf.lua
+++ nvim/.config/nvim/lua/nvim-tree_conf.lua
@@ -1,2 +1,3 @@
 require("nvim-tree").setup()
+require('nvim-tree.view').View.winopts.signcolumn = 'no'
 vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, desc = 'NvimTree Toggle' })
