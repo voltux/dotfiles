8f54e1aadf4e6903f7e38fd204cae00493d61e77
voltux <ioannis.arapis@ovhcloud.com>
2022-12-26 14:11:55 +0100

    [nvim] nvim-tree adapt size to contents

--- nvim/.config/nvim/lua/nvim-tree_conf.lua
+++ nvim/.config/nvim/lua/nvim-tree_conf.lua
@@ -1,3 +1,7 @@
-require("nvim-tree").setup()
+require("nvim-tree").setup {
+  view = {
+    adaptive_size = true,
+  }
+}
 require('nvim-tree.view').View.winopts.signcolumn = 'no'
 vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeToggle<CR>', { noremap = true, desc = 'NvimTree Toggle' })
