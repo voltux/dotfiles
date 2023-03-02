3f7921eba70197a00390b841d98410a3cd093373
voltux <ioannis.arapis@ovhcloud.com>
2023-03-02 11:11:52 +0100

    [nvim] add current context to indent blankline

--- nvim/.config/nvim/lua/indent-blankline_conf.lua
+++ nvim/.config/nvim/lua/indent-blankline_conf.lua
@@ -2,5 +2,6 @@ vim.api.nvim_set_keymap('n', '<leader>pi', ':IndentBlanklineToggle<CR>',
     { noremap = true, desc = 'IndentBlankLine Toggle' })
 require('indent_blankline').setup {
     filetype_exclude = { 'help', 'alpha', 'dashboard', 'neo-tree', 'Trouble', 'lazy', 'NvimTree'},
+    show_current_context = true,
 }
 vim.g.indent_blankline_enabled = false
