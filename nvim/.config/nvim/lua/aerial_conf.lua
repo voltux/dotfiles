f4ffb508c6d8abdc35a7df72d28e61d20c451be5
voltux <ioannis.arapis@ovhcloud.com>
2023-03-16 14:05:48 +0100

    [neogen] add keymap for documentation creation

--- nvim/.config/nvim/lua/aerial_conf.lua
+++ nvim/.config/nvim/lua/aerial_conf.lua
@@ -1,9 +1,10 @@
 require('aerial').setup({
   -- optionally use on_attach to set keymaps when aerial has attached to a buffer
   on_attach = function(bufnr)
-    vim.keymap.set('n', '[a', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-    vim.keymap.set('n', ']a', '<cmd>AerialNext<CR>', {buffer = bufnr})
+    vim.keymap.set('n', '[a', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+    vim.keymap.set('n', ']a', '<cmd>AerialNext<CR>', { buffer = bufnr })
   end
 })
 -- You probably also want to set a keymap to toggle aerial
-vim.keymap.set('n', '<leader>m', '<cmd>AerialToggle! right<CR>')
+vim.api.nvim_set_keymap('n', '<leader>m', '<cmd>AerialToggle! right<CR>',
+  { noremap = true, desc = 'Aerial minimap Toggle' })
--- nvim/.config/nvim/lua/neogen_conf.lua
+++ nvim/.config/nvim/lua/neogen_conf.lua
@@ -1,3 +1,5 @@
+vim.api.nvim_set_keymap('n', '<leader>c"', '<cmd>Neogen<CR>',
+    { noremap = true, desc = 'Neogen create documentation' })
 require('neogen').setup {
   enabled = true,
   snippet_engine = "luasnip",
