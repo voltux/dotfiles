328aa53974d2d913fe423f4b4fec59d422d38639
voltux <voltux@falcon.com>
2023-03-19 21:08:02 +0100

    [nvim] update gruvbox with new italic config

--- nvim/.config/nvim/lua/gruvbox_conf.lua
+++ nvim/.config/nvim/lua/gruvbox_conf.lua
@@ -1,20 +1,28 @@
 require("gruvbox").setup({
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = true,
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true, -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    overrides = {
-        SignColumn = { bg = 'NONE' },
-        LineNr = { bg = 'NONE' },
-        Error = { bg = 'NONE' },
-        ErrorMsg = { bg = 'NONE', fg = '#fb4934' },
-    },
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {
+    SignColumn = { bg = 'NONE' },
+    LineNr = { bg = 'NONE' },
+    Error = { bg = 'NONE' },
+    ErrorMsg = { bg = 'NONE', fg = '#fb4934' },
+  },
+  dim_inactive = false,
+  transparent_mode = false,
 })
 vim.cmd [[ colorscheme gruvbox ]] --keeping it here instead of vim_conf.lua because the plugin needs to be configured before the call
