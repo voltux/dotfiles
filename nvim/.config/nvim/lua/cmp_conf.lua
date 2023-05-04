2e237d7d7f255548c7e72cd6035d0ea88b97f366
voltux <ioannis.arapis@ovhcloud.com>
2023-05-04 11:01:29 +0200

    [nvim] cmp suggestions do not autoselect first entry unless explicitly selected

--- nvim/.config/nvim/lua/cmp_conf.lua
+++ nvim/.config/nvim/lua/cmp_conf.lua
@@ -36,7 +36,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
             if luasnip.jumpable(1) then
                 luasnip.jump(1)
