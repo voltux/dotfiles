139d66790e0f00ccd72ad036b3f4b5638f6658e1
voltux <ioannis.arapis@ovhcloud.com>
2023-05-04 11:02:29 +0200

    [nvim] add telescope bindings to neorg

--- nvim/.config/nvim/lua/neorg_conf.lua
+++ nvim/.config/nvim/lua/neorg_conf.lua
@@ -64,3 +64,19 @@ neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, key
         noremap = true,
     })
 end)
+vim.keymap.set("n", "<leader>fnh", "<cmd>Telescope neorg search_headings<cr>",
+    { noremap = true, desc = 'Telescope neorg search headings' })
+vim.keymap.set("n", "<leader>fnL", "<cmd>Telescope neorg insert_link<cr>",
+    { noremap = true, desc = 'Telescope neorg insert link' })
+vim.keymap.set("n", "<leader>fnl", "<cmd>Telescope neorg find_linkable<cr>",
+    { noremap = true, desc = 'Telescope neorg find linkable' })
+vim.keymap.set("n", "<leader>fnf", "<cmd>Telescope neorg find_norg_files<cr>",
+    { noremap = true, desc = 'Telescope neorg find linkable' })
+vim.keymap.set("n", "<leader>fnF", "<cmd>Telescope neorg insert_file_link<cr>",
+    { noremap = true, desc = 'Telescope neorg insert link' })
+vim.keymap.set("n", "<leader>fnp", "<cmd>Telescope neorg find_project_tasks<cr>",
+    { noremap = true, desc = 'Telescope neorg find project tasks' })
+vim.keymap.set("n", "<leader>fnt", "<cmd>Telescope neorg find_context_tasks<cr>",
+    { noremap = true, desc = 'Telescope neorg find context tasks' })
+vim.keymap.set("n", "<leader>fnw", "<cmd>Telescope neorg switch_workspace<cr>",
+    { noremap = true, desc = 'Telescope neorg switch workspace' })
