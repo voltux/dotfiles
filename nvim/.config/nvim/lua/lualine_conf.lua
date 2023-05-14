517f684c6ba26426957957cc2fc67056989a21ba
voltux <voltux@falcon.com>
2023-05-15 00:03:14 +0200

    [nvim] lualine custom theme

--- nvim/.config/nvim/lua/lualine_conf.lua
+++ nvim/.config/nvim/lua/lualine_conf.lua
@@ -1,6 +1,49 @@
+local colors = {
+  red      = '#fb4934',
+  aqua     = '#8ec07c',
+  purple   = '#d3869b',
+  green    = '#b8bb26',
+  blue     = '#83a598',
+  orange   = '#fe8019',
+  gray     = '#a89984',
+  darkgray = '#282828',
+}
+local gruvbox = {
+  normal = {
+    a = { bg = colors.blue, fg = colors.darkgray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.blue },
+    c = { bg = colors.darkgray, fg = colors.gray }
+  },
+  insert = {
+    a = { bg = colors.aqua, fg = colors.darkgray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.aqua },
+    c = { bg = colors.darkgray, fg = colors.gray }
+  },
+  visual = {
+    a = { bg = colors.purple, fg = colors.darkgray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.purple },
+    c = { bg = colors.darkgray, fg = colors.gray }
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.darkgray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.red },
+    c = { bg = colors.darkgray, fg = colors.gray }
+  },
+  command = {
+    a = { bg = colors.orange, fg = colors.darkgray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.orange },
+    c = { bg = colors.darkgray, fg = colors.gray }
+  },
+  inactive = {
+    a = { bg = colors.darkgray, fg = colors.gray, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.gray },
+    c = { bg = colors.darkgray, fg = colors.gray }
+  },
+}
+
 require('lualine').setup {
   options = {
-    theme = 'codedark',
+    theme = gruvbox,
     component_separators = '',
     section_separators = { left = '', right = '' },
   },
