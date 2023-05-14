47e0fca0cd9d1aa413e8dd05b33ca3035d00cfa2
voltux <voltux@falcon.com>
2023-05-14 19:56:45 +0200

    [nvim] calibrate gruvbox

--- nvim/.config/nvim/lua/gruvbox_conf.lua
+++ nvim/.config/nvim/lua/gruvbox_conf.lua
@@ -1,12 +1,13 @@
+local colors = require("gruvbox.palette").get_base_colors(vim.o.background, "hard")
 require("gruvbox").setup({
   undercurl = true,
   underline = true,
-  bold = true,
+  bold = false,
   italic = {
-    strings = true,
-    comments = true,
+    strings = false,
+    comments = false,
     operators = false,
-    folds = true,
+    folds = false,
   },
   strikethrough = true,
   invert_selection = false,
@@ -15,13 +16,17 @@ require("gruvbox").setup({
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
   contrast = "hard", -- can be "hard", "soft" or empty string
+  palette_overrides = {
+    dark0 = "#1d2021"
+  },
   overrides = {
     SignColumn = { bg = 'NONE' },
     LineNr = { bg = 'NONE' },
     Error = { bg = 'NONE' },
     ErrorMsg = { bg = 'NONE', fg = '#fb4934' },
+    NormalFloat = { fg = colors.fg1, bg = colors.bg0 },
   },
   dim_inactive = false,
-  transparent_mode = true,
+  transparent_mode = false,
 })
 vim.cmd [[ colorscheme gruvbox ]] --keeping it here instead of vim_conf.lua because the plugin needs to be configured before the call
