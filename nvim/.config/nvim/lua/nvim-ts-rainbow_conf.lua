63421b870ed3a2ccb3f90cc25aa740154652c6c6
voltux <ioannis.arapis@ovhcloud.com>
2023-05-17 09:42:50 +0200

    [nvim] nvim-ts-rainbow color calibration to match colorscheme

--- nvim/.config/nvim/lua/nvim-ts-rainbow_conf.lua
+++ nvim/.config/nvim/lua/nvim-ts-rainbow_conf.lua
@@ -1,1 +1,14 @@
+require("nvim-treesitter.configs").setup {
+    rainbow = {
+        hlgroups = {
+            'TSRainbowBlue',
+            'TSRainbowOrange',
+            'TSRainbowCyan',
+            'TSRainbowYellow',
+            'TSRainbowViolet',
+            'TSRainbowGreen',
+            'TSRainbowRed',
+        },
+    }
+}
 vim.g.rainbow_active = 1
