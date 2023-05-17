351417798799a7893853eed3b0ad0349a6e79a52
voltux <ioannis.arapis@ovhcloud.com>
2023-05-17 12:08:15 +0200

    [nvim] toggleterm change layout for floating windows & add mapping for htop

--- nvim/.config/nvim/lua/toggleterm_conf.lua
+++ nvim/.config/nvim/lua/toggleterm_conf.lua
@@ -2,13 +2,15 @@ require('toggleterm').setup {
     shade_terminals = false,
     float_opts = {
         border = "rounded",
+        height = vim.o.lines - 5,
+        width = vim.o.columns,
     }
 }
-vim.keymap.set("n", "<leader>sf", "<cmd>ToggleTerm direction=float border=rounded<cr>",
+vim.keymap.set("n", "<leader>sf", "<cmd>ToggleTerm direction=float<cr>",
     { silent = true, noremap = true, desc = 'Toggle floating terminal' })
-vim.keymap.set("n", "<leader>sl", "<cmd>ToggleTerm direction=vertical size=120<cr>",
+vim.keymap.set("n", "<leader>sl", "<cmd>ToggleTerm direction=vertical<cr>",
     { silent = true, noremap = true, desc = 'Toggle vertical terminal' })
-vim.keymap.set("n", "<leader>sj", "<cmd>ToggleTerm direction=horizontal size=40<cr>",
+vim.keymap.set("n", "<leader>sj", "<cmd>ToggleTerm direction=horizontal<cr>",
     { silent = true, noremap = true, desc = 'Toggle horizontal terminal' })
 vim.keymap.set("n", "<leader>st", "<cmd>ToggleTerm direction=tab<cr>",
     { silent = true, noremap = true, desc = 'Toggle tab terminal' })
@@ -22,6 +24,8 @@ local lazygit = require('toggleterm.terminal').Terminal:new({
     direction = "float",
     float_opts = {
         border = "rounded",
+        height = vim.o.lines - 5,
+        width = vim.o.columns,
     },
     -- function to run on opening the terminal
     on_open = function(term)
@@ -29,14 +33,36 @@ local lazygit = require('toggleterm.terminal').Terminal:new({
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
     end,
     -- function to run on closing the terminal
-    on_close = function(term)
-        vim.cmd("startinsert!")
-    end,
+    -- on_close = function(term)
+    --     vim.cmd("startinsert!")
+    -- end,
 })
 
 function LazygitToggle()
     lazygit:toggle()
 end
 
+local htop = require('toggleterm.terminal').Terminal:new({
+    cmd = "htop",
+    count = 5,
+    direction = "float",
+    float_opts = {
+        border = "rounded",
+        height = vim.o.lines - 5,
+        width = vim.o.columns,
+    },
+    -- function to run on opening the terminal
+    on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+    end,
+})
+
+function HtopToggle()
+    htop:toggle()
+end
+
 vim.keymap.set("n", "<leader>gg", "<cmd>lua LazygitToggle()<CR>",
-{ noremap = true, silent = true, desc = 'Toggle lazygit' })
+    { noremap = true, silent = true, desc = 'Toggle lazygit' })
+vim.keymap.set("n", "<leader>gh", "<cmd>lua HtopToggle()<CR>",
+    { noremap = true, silent = true, desc = 'Toggle htop' })
