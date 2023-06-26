05f7064a6a89dfd3b940696bda7301a93f6f73d0
voltux <ioannis.arapis@ovhcloud.com>
2023-06-26 15:34:30 +0200

    [nvim] nvim-tree switch root to cwd + focus on current file

--- nvim/.config/nvim/lua/nvim-tree_conf.lua
+++ nvim/.config/nvim/lua/nvim-tree_conf.lua
@@ -7,7 +7,14 @@ require("nvim-tree").setup {
     indent_markers = {
       enable = true,
     }
-  }
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = false,
+  },
+  sync_root_with_cwd = true,
 }
+
 require('nvim-tree.view').View.winopts.signcolumn = 'no'
 vim.keymap.set('n', '<leader>n', '<cmd>NvimTreeToggle<CR>', { noremap = true, desc = 'NvimTree Toggle' })
+vim.g.nvim_tree_respect_buf_cwd = 1
