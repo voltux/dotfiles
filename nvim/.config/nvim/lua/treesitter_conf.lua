c2f402dd0c0ec2a2378bd86cdaa617c628d29d1c
voltux <voltux@laptop1478108>
2024-01-29 10:16:27 +0100

    [nvim] remove markdown treesitter highlight for the moment, it is buggy

--- nvim/.config/nvim/lua/treesitter_conf.lua
+++ nvim/.config/nvim/lua/treesitter_conf.lua
@@ -7,6 +7,8 @@ require 'nvim-treesitter.configs'.setup {
     auto_install = false,
     highlight = {
         enable = true,
+        disable = { "markdown" },
+        additional_vim_regex_highlighting = true,
     },
     rainbow = {
         enable = true,
