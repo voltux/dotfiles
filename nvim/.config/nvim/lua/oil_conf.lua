e5011e34ad304ac41663163fc2a9667b2d77a956
voltux <voltux@laptop1478108>
2024-02-19 09:18:34 +0100

    [nvim] unset oil as default file manager
    this created a problem when downloading spell files, as it needed netrw

--- nvim/.config/nvim/lua/oil_conf.lua
+++ nvim/.config/nvim/lua/oil_conf.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<leader>o", require("oil").open, { desc = "Oil open current directory" })
-require('oil').setup()
+require('oil').setup({
+    default_file_explorer = false,
+})
