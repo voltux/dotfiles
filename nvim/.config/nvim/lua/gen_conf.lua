539fc4cce875940606256e19256058ad6f922679
voltux <voltux@laptop1478108>
2024-04-12 10:17:54 +0200

    [nvim] update lazy-lock

--- nvim/.config/nvim/lua/gen_conf.lua
+++ nvim/.config/nvim/lua/gen_conf.lua
@@ -1,12 +1,13 @@
 require('gen').setup({
-    model = "mixtral",   -- The default model to use.
-    host = "localhost",  -- The host running the Ollama service.
-    quit_map = "q",      -- set keymap for close the response window
-    retry_map = "<c-r>", -- set keymap to re-send the current prompt
+    model = "mixtral",                                                      -- The default model to use.
+    host = "768bd4d3-493b-4641-af3c-3615d41425cc.app.gra.ai.cloud.ovh.net", -- The host running the Ollama service.
+    quit_map = "q",                                                         -- set keymap for close the response window
+    retry_map = "<c-r>",                                                    -- set keymap to re-send the current prompt
     command = function(options)
         local body = { model = options.model, stream = true }
         return
-            'curl --silent --no-buffer -X POST https://' .. options.host .. "/api/chat -d $body"
+            'curl -H "Authorization: Bearer VaFJSLUGQ+6XFUHtL4FL4bbah/K0NwGT4pEci4y3B4B4+pAgWTiiU/+BpLpqXpmc" --no-buffer -X POST https://' ..
+            options.host .. "/api/chat -d $body"
     end,
     -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
     -- This can also be a command string.
