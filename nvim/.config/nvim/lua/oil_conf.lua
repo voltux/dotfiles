d917677912dcad13fe2e8827e22c0cfde4cd2506
voltux <voltux@laptop1478108>
2024-05-16 09:52:13 +0200

    [nvim] set <leader>o and <leader>fo as (Ne)org prefix, move oil to <leader>fO

--- nvim/.config/nvim/lua/oil_conf.lua
+++ nvim/.config/nvim/lua/oil_conf.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>o", require("oil").open, { desc = "Oil open current directory" })
+vim.keymap.set("n", "<leader>fO", require("oil").open, { desc = "Oil open current directory" })
 require('oil').setup({
     default_file_explorer = false,
 })
