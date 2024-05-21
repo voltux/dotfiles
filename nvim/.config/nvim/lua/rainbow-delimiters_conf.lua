353b4ac323fccf642710cf0294c976d498c4899f
voltux <voltux@laptop1478108>
2024-05-21 15:26:33 +0200

    [nvim] replace deprecated nvim-ts-rainbow with rainbow-delimiters

---
+++ nvim/.config/nvim/lua/rainbow-delimiters_conf.lua
@@ -0,0 +1,11 @@
+require("rainbow-delimiters.setup").setup {
+    highlight = {
+        'TSRainbowYellow',
+        'TSRainbowCyan',
+        'TSRainbowOrange',
+        'TSRainbowViolet',
+        'TSRainbowRed',
+        'TSRainbowBlue',
+        'TSRainbowGreen',
+    },
+}
