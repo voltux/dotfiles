8a7173c677f5d1addf6ba90eb9c923139b2655a6
voltux <voltux@laptop1478108>
2024-07-18 17:59:02 +0200

    [nvim] update which-key conf to trigger exclusively in normal mode

--- nvim/.config/nvim/lua/which-key_conf.lua
+++ nvim/.config/nvim/lua/which-key_conf.lua
@@ -1,17 +1,17 @@
 local wk = require('which-key')
 wk.add({
-    { "<leader>f", group = "find" },
-    { "<leader>c", group = "code" },
-    { "<leader>l", group = "language" },
-    { "<leader>c", group = "check spelling" },
-    { "<leader>s", group = "shell (toggleterm)" },
-    { "<leader>S", group = "shell (native)" },
-    { "<leader>w", group = "whitespace" },
-    { "<leader>x", group = "trouble" },
-    { "<leader>g", group = "git" },
-    { "<leader>p", group = "plugin" },
-    { "<leader>t", group = "table mode" },
-    { "<leader>o", group = "Neorg" },
+    { "<leader>f", group = "find",               mode = "n" },
+    { "<leader>c", group = "code",               mode = "n" },
+    { "<leader>l", group = "language",           mode = "n" },
+    { "<leader>c", group = "check spelling",     mode = "n" },
+    { "<leader>s", group = "shell (toggleterm)", mode = "n" },
+    { "<leader>S", group = "shell (native)",     mode = "n" },
+    { "<leader>w", group = "whitespace",         mode = "n" },
+    { "<leader>x", group = "trouble",            mode = "n" },
+    { "<leader>g", group = "git",                mode = "n" },
+    { "<leader>p", group = "plugin",             mode = "n" },
+    { "<leader>t", group = "table mode",         mode = "n" },
+    { "<leader>o", group = "Neorg",              mode = "n" },
 })
 wk.setup {
     win = {
@@ -20,4 +20,5 @@ wk.setup {
     disable = {
         filetypes = { 'TelescopePrompt', 'dashboard' }
     },
+    triggers = { "<auto>", mode = "n" }
 }
