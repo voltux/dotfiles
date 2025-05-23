c1623c385bb1229b0779205bfebae363c12dcc78
voltux <voltux@laptop1478108>
2025-05-23 15:03:01 +0200

    [nvim] fix(doc): fix dap command documentation

--- nvim/.config/nvim/lua/dap_conf.lua
+++ nvim/.config/nvim/lua/dap_conf.lua
@@ -3,5 +3,5 @@ vim.keymap.set('n', '<F5>', '<Cmd>lua require"dap".toggle_breakpoint()<CR>',
     { noremap = true, desc = "Dap debugger toggle breakpoint" })
 vim.keymap.set('n', '<F9>', '<Cmd>lua require"dap".continue()<CR>', { noremap = true, desc = "Dap debugger continue" })
 vim.keymap.set('n', '<F10>', '<Cmd>lua require"dap".step_over()<CR>', { noremap = true, desc = "Dap debugger step over" })
-vim.keymap.set('n', '<F11>', '<Cmd>lua require"dap".step_into()<CR>', { noremap = true, desc = "Dap debugger step over" })
-vim.keymap.set('n', '<F12>', '<Cmd>lua require"dap".step_out()<CR>', { noremap = true, desc = "Dap debugger step over" })
+vim.keymap.set('n', '<F11>', '<Cmd>lua require"dap".step_into()<CR>', { noremap = true, desc = "Dap debugger step into" })
+vim.keymap.set('n', '<F12>', '<Cmd>lua require"dap".step_out()<CR>', { noremap = true, desc = "Dap debugger step out" })
