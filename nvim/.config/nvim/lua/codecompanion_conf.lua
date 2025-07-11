4fcf3424e6438373f23625240674ac3449195054
voltux <voltux@laptop1478108>
2025-07-11 11:39:20 +0200

    [nvim] codecompanion: get model url from env

--- nvim/.config/nvim/lua/codecompanion_conf.lua
+++ nvim/.config/nvim/lua/codecompanion_conf.lua
@@ -3,7 +3,7 @@ require("codecompanion").setup({
     ollama = function()
       return require("codecompanion.adapters").extend("openai_compatible", {
         env = {
-          url = "https://mamba-codestral-7b-v0-1.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat",
+          url = os.getenv("LLM_URL"),
           api_key = os.getenv("LLM_TOKEN"),
           chat_url = "/v1/chat/completions",
         },
