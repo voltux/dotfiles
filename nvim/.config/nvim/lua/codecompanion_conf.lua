6a9be55204e11e6631577e9d501191c9fe2a97e9
voltux <voltux@laptop1478108>
2025-08-28 17:50:02 +0200

    [nvim] update lazy-lock + update resulting deprecated config in codecompanion

--- nvim/.config/nvim/lua/codecompanion_conf.lua
+++ nvim/.config/nvim/lua/codecompanion_conf.lua
@@ -1,18 +1,20 @@
 require("codecompanion").setup({
   adapters = {
-    ollama = function()
-      return require("codecompanion.adapters").extend("openai_compatible", {
-        env = {
-          url = os.getenv("LLM_URL"),
-          api_key = os.getenv("LLM_TOKEN"),
-          chat_url = "/v1/chat/completions",
-        },
-        headers = {
-          ["Content-Type"] = "application/json",
-          ["Authorization"] = "Bearer " .. os.getenv("LLM_TOKEN"),
-        },
-      })
-    end,
+    http = {
+      ollama = function()
+        return require("codecompanion.adapters").extend("openai_compatible", {
+          env = {
+            url = os.getenv("LLM_URL"),
+            api_key = os.getenv("LLM_TOKEN"),
+            chat_url = "/v1/chat/completions",
+          },
+          headers = {
+            ["Content-Type"] = "application/json",
+            ["Authorization"] = "Bearer " .. os.getenv("LLM_TOKEN"),
+          },
+        })
+      end,
+    },
   },
   strategies = {
     chat = {
@@ -25,6 +27,6 @@ require("codecompanion").setup({
 })
 
 vim.keymap.set('n', '<leader>aa', '<cmd>CodeCompanionChat Toggle<CR>',
-    { noremap = true, desc = 'Codecompanion chat toggle' })
+  { noremap = true, desc = 'Codecompanion chat toggle' })
 vim.keymap.set('n', '<leader>ac', '<cmd>CodeCompanionActions<CR>',
-    { noremap = true, desc = 'Codecompanion actions' })
+  { noremap = true, desc = 'Codecompanion actions' })
