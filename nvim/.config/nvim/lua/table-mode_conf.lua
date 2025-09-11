dde94100eb033ea60ca53ba6084837d370814325
voltux <voltux@laptop1478108>
2025-09-11 11:55:59 +0200

    [nvim] adjust table mode corner on filetype

--- nvim/.config/nvim/lua/table-mode_conf.lua
+++ nvim/.config/nvim/lua/table-mode_conf.lua
@@ -1,2 +1,3 @@
-vim.g.table_mode_corner='|'
+vim.cmd[[ autocmd BufNewFile,BufRead *.org let g:table_mode_corner="+" ]]
+vim.cmd[[ autocmd BufNewFile,BufRead *.(md|neorg) let g:table_mode_corner="|" ]]
 vim.keymap.set("n", "<leader>tt", "<cmd>TableModeToggle<CR>", { desc = "Table mode toggle" })
