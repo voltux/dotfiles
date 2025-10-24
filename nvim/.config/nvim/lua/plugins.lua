f6edd4caf61d1abcbb6ce605145333a60f96b156
voltux <voltux@laptop1478108>
2025-10-24 14:26:11 +0200

    [nvim] add neotree to play with

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -20,6 +20,16 @@ local plugins = {
         version = "v1.13.0"
     },
     {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+    },
+    {
         -- filetype icons
         'nvim-tree/nvim-web-devicons',
         config = function() require('nvim-web-devicons_conf') end,
