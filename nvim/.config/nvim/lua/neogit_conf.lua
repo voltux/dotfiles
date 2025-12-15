be3f7845a1b3bac59424fd076f6e42515871bafa
voltux <voltux@laptop1478108>
2025-12-15 23:22:34 +0100

    [nvim] diff with diffview on neogit

--- nvim/.config/nvim/lua/neogit_conf.lua
+++ nvim/.config/nvim/lua/neogit_conf.lua
@@ -1,3 +1,7 @@
-require('neogit').setup({})
+require('neogit').setup({
+    integrations = {
+        diffview = true
+    }
+})
 vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>",
     { noremap = true, silent = true, desc = 'Toggle Neogit' })
