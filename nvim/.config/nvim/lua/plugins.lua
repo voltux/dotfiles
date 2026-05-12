74abdb86ec5f1fa0c2a3976ad3857f8af5574532
voltux <voltux@laptop1478108>
2026-05-12 14:24:12 +0200

    [nvim] disable markview.nvim until it becomes compatible with nvim v0.12.x

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -570,24 +570,24 @@ local plugins = {
             'saghen/blink.cmp',              -- autocompletion
             "stevearc/dressing.nvim",        -- for input provider dressing
             "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-            "OXY2DEV/markview.nvim",         -- markdown preview
-            {
-                -- support for image pasting
-                "HakonHarnes/img-clip.nvim",
-                event = "VeryLazy",
-                opts = {
-                    -- recommended settings
-                    default = {
-                        embed_image_as_base64 = false,
-                        prompt_for_file_name = false,
-                        drag_and_drop = {
-                            insert_mode = true,
-                        },
-                        -- required for Windows users
-                        use_absolute_path = true,
-                    },
-                },
-            },
+            -- "OXY2DEV/markview.nvim",         -- markdown preview TODO: uncomment it when it becomes compatible with nvim 0.12.x
+            -- {
+            --     -- support for image pasting
+            --     "HakonHarnes/img-clip.nvim",
+            --     event = "VeryLazy",
+            --     opts = {
+            --         -- recommended settings
+            --         default = {
+            --             embed_image_as_base64 = false,
+            --             prompt_for_file_name = false,
+            --             drag_and_drop = {
+            --                 insert_mode = true,
+            --             },
+            --             -- required for Windows users
+            --             use_absolute_path = true,
+            --         },
+            --     },
+            -- },
         },
         config = function() require('avante_conf') end,
     },
@@ -626,12 +626,12 @@ local plugins = {
             })
         end
     },
-    {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-        branch = "main",
-        config = function() require('markview_conf') end,
-    }
+    -- {
+    --     "OXY2DEV/markview.nvim", TODO: uncomment it when it becomes compatible with nvim 0.12.x
+    --     lazy = false,
+    --     branch = "main",
+    --     config = function() require('markview_conf') end,
+    -- }
 }
 
 local opts = {
